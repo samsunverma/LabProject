307f84dca2aa05b6cbbad3d6db6057ece1510159
samsunverma <samsunverma2013@gmail.com>
2024-11-29 00:48:13 -0500

    Working with JSON

--- script.js
+++ script.js
@@ -12,4 +12,14 @@ const student = {
   console.log("Name:", student.name);
   console.log("Age:", student.age);
   console.log("Info:", student.displayInfo());
-  
+   
+  //Part 2: Working with JSON
+   const studentJSON = JSON.stringify(student);
+   console.log("JSON String: ", studentJSON);
+
+   const parsedStudent = JSON.parse(studentJSON);
+   console.log("Parsed Object: ", parsedStudent);
+   console.log("Are objects Identical?", student === parsedStudent);
+   
+   
+   
